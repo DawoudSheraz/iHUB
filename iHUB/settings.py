637a1b694c31da9023ad8b0f3a10be390f9b0e2d
DawoudSheraz <dawoud.sheraz@arbisoft.com>
2018-07-19 10:21:26 +0500

    Redo

--- iHUB/settings.py
+++ iHUB/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z5^1tr_pez$eyy_+#eov1^%+-u_6du_egooi5ts^ym9k^&x1x$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
