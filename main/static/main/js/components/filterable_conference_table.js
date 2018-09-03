d98ae8a42a68d7fa0d76e7eec488dcfd20f341b0
DawoudSheraz <dawoud.sheraz@arbisoft.com>
2018-09-03 14:41:09 +0500

    URL bubbling to parent component added

--- main/static/main/js/components/filterable_conference_table.js
+++ main/static/main/js/components/filterable_conference_table.js
@@ -6,15 +6,23 @@ class FilterableConferenceTable extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      'search_text': ''
+      'search_text': '',
+      'req_url': this.props.base_url,
     }
     this.handleTextChange = this.handleTextChange.bind(this)
+    this.new_request_url = this.new_request_url.bind(this)
   }
 
 
   handleTextChange(value){
     this.setState({
       'search_text': value,
+    })
+  }
+
+  new_request_url(value){
+    this.setState({
+      'req_url': value,
     })
   }
 
@@ -29,7 +37,9 @@ class FilterableConferenceTable extends React.Component{
         {/*  ConferenceList component, where data is passed by parent as prop*/}
         <ConferenceList conference_list = {this.props.conference_list} search_text={this.state.search_text}/>
 
-        <Pagination base_url = {this.props.base_url} pagination_data = {this.props.pagination_data}/>
+        <Pagination base_url = {this.props.base_url} pagination_data = {this.props.pagination_data} NewRequestUrl={this.new_request_url}/>
+
+        <p> AJAX Request URL : {this.state.req_url}</p>
 
       </div>
     )
--- main/static/main/js/components/pagination_component.js
+++ main/static/main/js/components/pagination_component.js
@@ -4,16 +4,20 @@ class Pagination extends React.Component{
 
   constructor(props){
     super(props);
+    this.checkInput = this.checkInput.bind(this);
+    this.sendButtonUrlToParent = this.sendButtonUrlToParent.bind(this)
   }
 
+  // url/button that is clickable
   get_active_li_item(href_link, text){
     return(
       <li className="page-item">
-        <a className="page-link" href={href_link}>{text}</a>
+        <a className="page-link" href={href_link} onClick={this.sendButtonUrlToParent}>{text}</a>
       </li>
     )
   }
 
+// Disabled buttons e.g. next button on last page
   get_disable_li_item(text){
     return(
     <li className="page-item disabled">
@@ -22,16 +26,36 @@ class Pagination extends React.Component{
   )
   }
 
+  sendButtonUrlToParent(e){
+    // Stop the re-direction by <a> tag
+    e.preventDefault();
+    this.props.NewRequestUrl(e.target.href)
+
+  }
+
+  // The input should remain between the min and max
   checkInput(e){
-    console.log(e.target.value)
+    let out_val = e.target.value;
+
+    if(out_val < parseInt(e.target.min)){
+      out_val = e.target.min
+    }
+    if(out_val > parseInt(e.target.max)){
+      out_val = e.target.max
+    }
+    out_val = this.props.base_url + '?page=' + out_val
+    this.props.NewRequestUrl(out_val)
+
   }
 
   render(){
+    // Buttons whose state is decided based on the current page number
     let prev_button, next_button, first_button, last_button;
 
     let max_page = this.props.pagination_data['pages']
     let current = this.props.pagination_data['current_page']
     let base_url = this.props.base_url
+
     // On the first page
     if(this.props.pagination_data['previous']==null){
       first_button = this.get_disable_li_item('First')
@@ -39,7 +63,7 @@ class Pagination extends React.Component{
     }
     else{
       first_button = this.get_active_li_item(base_url + '?page=1', 'First')
-      prev_button = this.get_active_li_item(base_url + '?page='+ (current - 1).toString(), 'Previous')
+      prev_button = this.get_active_li_item(base_url + '?page='+ (current - 1), 'Previous')
     }
 
     // On the Last  page
@@ -48,8 +72,8 @@ class Pagination extends React.Component{
       last_button = this.get_disable_li_item('Last')
     }
     else{
-      next_button = this.get_active_li_item(base_url + '?page='+ (current + 1).toString(), 'Next')
-      last_button = this.get_active_li_item(base_url + '?page='+ (max_page.toString()), 'Last')
+      next_button = this.get_active_li_item(base_url + '?page='+ (current + 1), 'Next')
+      last_button = this.get_active_li_item(base_url + '?page='+ (max_page), 'Last')
     }
 
     return(
@@ -59,7 +83,7 @@ class Pagination extends React.Component{
           {prev_button}
 
           <li className="page-item">
-            <span>Page <input type='number' onBlur={this.checkInput}  min='1' max={max_page}></input> of {max_page}</span>
+            <span>Page <input type='number' defaultValue={current} onBlur={this.checkInput}  min='1' max={max_page}></input> of {max_page}</span>
           </li>
 
           {next_button}
@@ -74,5 +98,6 @@ class Pagination extends React.Component{
 
 Pagination.defaultProps = {
   base_url: '',
+  NewRequestUrl: () => null,
 
 }
