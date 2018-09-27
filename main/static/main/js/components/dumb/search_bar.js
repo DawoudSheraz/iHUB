3a11a729ae14909ea9e1ba77258bb0892881f558
DawoudSheraz <dawoud.sheraz@arbisoft.com>
2018-09-27 16:28:19 +0500

    autocomplete feature added on skills search(Static Version)

--- main/static/main/js/components/dumb/search_bar.js
+++ main/static/main/js/components/dumb/search_bar.js
@@ -7,19 +7,48 @@ class ControlledSearchBar extends React.Component{
     super(props);
 
     this.updateState = this.updateState.bind(this)
+    this.searchRef = React.createRef()
+  }
+
+  componentDidMount(){
+    const current = this.searchRef.current
+
+    // Convert input to TokenField component
+    $(current).tokenfield({
+      autocomplete: {
+      source: ['python', 'django', 'music'],
+      delay: 100
+      }
+  })
+
+  // add onChange event listener via jQuery
+  $(current).change(function(){
+    this.props.onEditAction(current.value)
+  }.bind(this))
+
+  // Checking for duplicate tokens
+  $(current).on('tokenfield:createtoken', function (event) {
+    var existingTokens = $(this).tokenfield('getTokens');
+    $.each(existingTokens, function(index, token) {
+        if (token.value === event.attrs.value)
+            event.preventDefault();
+    });
+});
   }
 
   // Dispatch the new value to Store
   updateState(e){
     this.props.onEditAction(e.target.value)
-
   }
 
   render(){
 
     return(
       <div >
-      <input  type='input' placeholder='Search on Skills' onChange={this.updateState} value={this.props.search_text}></input>
+      <input  type='input' placeholder='Search on Skills'
+        onChange={this.updateState}
+        value={this.props.search_text}
+        ref={this.searchRef}></input>
       </div>
     )
 
